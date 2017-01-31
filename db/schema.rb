62efaff163616bb1ceffec7d9fb9ddac11a0fab2
YarekTenko <yarektenko@gmail.com>
2017-01-31 13:25:09 -0800

    Add password digest to User model
    Changed the user password from standard text to encrypted by adding
    bcrypt gem, introducing password_digest column and including
    has_secure_password method in the User model.
    
    /users now displays all registered Users' name, email and encrypted
    password.
    
    For signup, changed users/new to /signup in routes.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127000826) do
+ActiveRecord::Schema.define(version: 20170131210619) do
 
   create_table "bets", force: :cascade do |t|
     t.text     "bet"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170127000826) do
     t.string "email"
     t.string "password"
     t.text   "avatar"
+    t.string "password_digest"
   end
 
 end
