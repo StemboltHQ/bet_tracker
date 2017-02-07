c7414c21f6d4206b98e5ebc9b63dbeeaf2255963
YarekTenko <yarektenko@gmail.com>
2017-02-07 14:33:35 -0800

    Define resolve function for Bet
    Creates an entry in the Debt model that indicates which user owes who
    and how much.
    Returns the newly created Debt entries as an array.

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -3,6 +3,17 @@ class Bet < ApplicationRecord
   has_many :user_bets
   has_many :options, through: :user_bets, source: :bet_option
 
+  def resolve
+    debt_relationships = user_bets.winners.to_a.product(user_bets.losers.to_a)
+    debt_relationships.map do |(won_bet, lost_bet)|
+      Debt.create(
+        creditor: won_bet.user,
+        debtor: lost_bet.user,
+        amount:  amount_owed(lost_bet, won_bet)
+      )
+    end
+  end
+
   def winner_total
     @winner_total ||= user_bets.winners.map(&:amount_bet).sum
   end
