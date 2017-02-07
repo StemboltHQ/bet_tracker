f24f1479167b528e7dd0fa9128741537fd091719
YarekTenko <yarektenko@gmail.com>
2017-02-07 13:18:05 -0800

    Define winner_total method for Bet
    Calculates and returns the total amount of money that was bet by the
    winning side.

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -2,4 +2,8 @@ class Bet < ApplicationRecord
   has_many :users, through: :user_bets
   has_many :user_bets
   has_many :options, through: :user_bets, source: :bet_option
+
+  def winner_total
+    @winner_total ||= user_bets.winners.map(&:amount_bet).sum
+  end
 end
