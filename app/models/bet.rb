f1eeb3dcbc5a6bbf2d76429f62dc9d92410cf2de
YarekTenko <yarektenko@gmail.com>
2017-02-21 16:28:15 -0800

    Modify Bet: add winning_option param to resolve
    Passed option updates to be the winning one.

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -3,7 +3,8 @@ class Bet < ApplicationRecord
   has_many :user_bets
   has_many :options, through: :user_bets, source: :bet_option
 
-  def resolve
+  def resolve(winning_option:)
+    winning_option.update!(winner: true)
     debt_relationships = user_bets.winners.to_a.product(user_bets.losers.to_a)
     debt_relationships.map do |(won_bet, lost_bet)|
       Debt.create(
