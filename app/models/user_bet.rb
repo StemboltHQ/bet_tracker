cca2a2b8a85f1e0b3775010194ada2642b2aabf3
YarekTenko <yarektenko@gmail.com>
2017-03-10 09:31:26 -0800

    Add "Bet!" button for options in Bet
    Added the ability to select an option and input the desired betting
    amount.

--- app/models/user_bet.rb
+++ app/models/user_bet.rb
@@ -3,6 +3,8 @@ class UserBet < ApplicationRecord
   belongs_to :bet
   belongs_to :bet_option
 
+  accepts_nested_attributes_for :user
+
   scope :winners, -> { joins(:user, :bet_option).merge(BetOption.winners) }
   scope :losers, -> { joins(:user, :bet_option).merge(BetOption.losers) }
 end
