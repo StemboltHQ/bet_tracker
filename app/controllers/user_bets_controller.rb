7c51392ae1514b9511cf2a9ea202ef6f7e5887ae
YarekTenko <yarektenko@gmail.com>
2017-03-10 09:48:41 -0800

    Define create method for UserBetsController

--- app/controllers/user_bets_controller.rb
+++ app/controllers/user_bets_controller.rb
@@ -1,3 +1,19 @@
 class UserBetsController < ApplicationController
-  def create; end
+  def create
+    @bet = Bet.find(params[:bet_id])
+    @user_bet = @bet.user_bets.new(user_bet_params)
+    if @user_bet.save
+      flash[:success] = 'Your bet has been successfully placed!'
+    else
+      flash[:danger] = 'Couldn\'t place your bet.'
+    end
+    redirect_to bet_path(@bet)
+  end
+
+  private
+
+  def user_bet_params
+    params.require(:user_bet)
+          .permit(:user_id, :bet_id, :amount_bet, :bet_option_id)
+  end
 end
