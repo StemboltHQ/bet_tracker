ab422946e1c8deff73b87e7c839369d016e375c0
YarekTenko <yarektenko@gmail.com>
2017-03-16 15:46:42 -0700

    Fix bet_spec.rb
    -Added fields to Bet and BetOption that are now validated and required
    upon instantiation.
    -Added tests for all methods in Bet model.

--- spec/models/bet_spec.rb
+++ spec/models/bet_spec.rb
@@ -1,43 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Bet, type: :model do
-  let!(:bob) do
+  let(:bob) do
     User.create(username: 'Bob', email: 'bob@mail.com', password: '123')
   end
-  let!(:peter) do
+  let(:peter) do
     User.create(username: 'Peter', email: 'peter@mail.com', password: 'pete1')
   end
-  let!(:jack) do
+  let(:jack) do
     User.create(username: 'Jack', email: 'jack@mail.com', password: 'notjack')
   end
 
   let(:bet1) do
-    Bet.create(bet: 'Superbowl: Atlanta vs New England, place your bets!')
+    Bet.create(bet: 'Superbowl: Atlanta vs New England, place your bets!',
+               expires_at: DateTime.tomorrow, creator_id: bob.id)
   end
-  let(:option1) { BetOption.create(option_text: 'Atlanta wins') }
-  let(:option2) { BetOption.create(option_text: 'New England wins') }
-
-  let!(:users_place_bets) do
+  let(:option1) { BetOption.create(option_text: 'Atlanta wins', bet: bet1) }
+  let(:option2) do
+    BetOption.create(option_text: 'New England wins', bet: bet1)
+  end
+  let(:bob_bets_100_on_option1) do
     UserBet.create(user:       bob,
                    bet:        bet1,
                    bet_option: option1,
                    amount_bet: 100)
-
+  end
+  let(:peter_bets_50_on_option1) do
     UserBet.create(user:       peter,
                    bet:        bet1,
                    bet_option: option1,
                    amount_bet: 50)
-
+  end
+  let(:jack_bets_300_on_option2) do
     UserBet.create(user:       jack,
                    bet:        bet1,
                    bet_option: option2,
                    amount_bet: 300)
   end
+  let!(:users_place_bets) do
+    bob_bets_100_on_option1
+    peter_bets_50_on_option1
+    jack_bets_300_on_option2
+  end
 
   describe('#resolve') do
     subject(:debts) { bet1.resolve(winning_option: option) }
     let(:option) { option1 }
-    xit 'is an array of debts' do
+    it 'is an array of debts' do
       expect(debts).to all be_a Debt
     end
 
@@ -45,7 +54,7 @@ RSpec.describe Bet, type: :model do
       let(:expected_debt) do
         Debt.find_by!(debtor: jack, creditor: bob, amount: 201)
       end
-      xit 'contains the correct debts' do
+      it 'contains the correct debts' do
         expect(debts).to include expected_debt
       end
     end
@@ -55,7 +64,7 @@ RSpec.describe Bet, type: :model do
         Debt.find_by!(debtor: bob, creditor: jack, amount: 100)
       end
       let(:option) { option2 }
-      xit 'contains the correct debts' do
+      it 'contains the correct debts' do
         expect(debts).to include expected_debt
       end
     end
@@ -63,8 +72,23 @@ RSpec.describe Bet, type: :model do
 
   describe('#bet_total') do
     subject { bet1.bet_total }
-
     let(:expected_bet_total) { 450 }
-    xit { is_expected.to eq expected_bet_total }
+    it { is_expected.to eq expected_bet_total }
+  end
+
+  describe('#winner_total') do
+    subject { bet1.winner_total }
+    let!(:winning_option) { option1.update(winner: true) }
+    let(:expected_winner_total) { 150 }
+    it { is_expected.to eq expected_winner_total }
+  end
+
+  describe('#amount_owed') do
+    subject { bet1.amount_owed(lost_bet, won_bet) }
+    let!(:winning_option) { option2.update(winner: true) }
+    let(:lost_bet) { bob_bets_100_on_option1 }
+    let(:won_bet) { jack_bets_300_on_option2 }
+    let(:expected_amount_owed) { 100 }
+    it { is_expected.to eq expected_amount_owed }
   end
 end
