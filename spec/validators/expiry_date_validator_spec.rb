11fabde56af01212a1e81cec73a5880c29d91745
YarekTenko <yarektenko@gmail.com>
2017-03-21 14:42:13 -0700

    Add rspec for date_validator

---
+++ spec/validators/expiry_date_validator_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe ExpiryDateValidator, type: :validator do
+  describe('#validate') do
+    subject { bet }
+    let(:user) do
+      User.create(username: 'Ivan',
+                  email: 'imustbreakyou@mail.com',
+                  password: 'hammerandsickle')
+    end
+    let(:bet) do
+      Bet.new(bet: 'Test bet',
+              creator_id: user.id,
+              expires_at: expiry_date)
+    end
+    context 'valid expiry date' do
+      let(:expiry_date) { DateTime.tomorrow }
+      it { is_expected.to be_valid }
+    end
+    context 'invalid expiry date' do
+      let(:expiry_date) { DateTime.yesterday }
+      it { is_expected.to_not be_valid }
+    end
+  end
+end
