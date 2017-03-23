7a1035742d62aed89a1b3430d5e37b133168f2f9
YarekTenko <yarektenko@gmail.com>
2017-03-23 15:44:05 -0700

    Add ability to resolve debts

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -3,5 +3,10 @@ class DebtsController < ApplicationController
     @debts = current_user.debts
   end
 
-  def update; end
+  def update
+    @debt = Debt.find(params[:id])
+    @bet = Bet.find(params[:bet_id])
+    @debt.update(payment_date: DateTime.current)
+    redirect_to @bet
+  end
 end
