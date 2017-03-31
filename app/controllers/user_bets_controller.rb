f71830c0d5b2b390dbd3f7afbaa4439a43e149a7
YarekTenko <yarektenko@gmail.com>
2017-03-31 10:20:59 -0700

    Add verification when placing bet
    -Bet cannot be placed if the form is empty;
    -Bet cannod be placed if bet option is not chosen;
    -Bet cannot be placed if amount is invalid.

--- app/controllers/user_bets_controller.rb
+++ app/controllers/user_bets_controller.rb
@@ -1,4 +1,6 @@
 class UserBetsController < ApplicationController
+  before_action :verify_fields, only: :create
+
   def create
     @bet = Bet.find(params[:bet_id])
     @user_bet = @bet.user_bets.new(user_bet_params)
@@ -15,5 +17,37 @@ class UserBetsController < ApplicationController
   def user_bet_params
     params.require(:user_bet)
           .permit(:user_id, :bet_id, :amount_bet, :bet_option_id)
+  end
+
+  def verify_fields
+    bet = Bet.find(params[:bet_id])
+    successes = []
+    successes << verify_amount
+    successes << verify_bet_option
+    return unless successes.include?(false)
+    redirect_to bet_path(bet)
+  end
+
+  def verify_bet_option
+    if params[:user_bet][:bet_option_id].nil?
+      flash[:danger] = 'Please pick an option before waging on it.'
+      false
+    else
+      true
+    end
+  end
+
+  def verify_amount
+    if !valid_amount?(params[:user_bet][:amount_bet])
+      flash[:danger] = 'Please enter a valid amount before placing your bet.'
+      false
+    else
+      true
+    end
+  end
+
+  def valid_amount?(amount)
+    valid_amount_pattern = /^(\d+)(\.?)(\d\d?)$/
+    amount.match(valid_amount_pattern).nil? ? false : true
   end
 end
